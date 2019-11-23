a1e08721a31763b0081601eae5521a79056ea4de
kiprotichdominic <kiprotichdominic102@gmail.com>
2019-11-23 20:24:46 +0300

    fix changes

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -44,6 +44,13 @@ export class QuoteComponent implements OnInit {
     this.quotes[index].showDescription = !this.quotes[index].showDescription;
   }
 
+  addNewQuote(quote) {
+    let quoteLength = this.quotes.length;
+    quote.id = quoteLength + 1;
+    quote.completeDate = new Date(quote.completeDate);
+    this.quotes.push(quote);
+  }
+
   deleteQuote(isComplete, index) {
     if (isComplete) {
       let toDelete = confirm(
