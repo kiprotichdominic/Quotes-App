6edabb3b2f1a47a0f2a4ea00cfa9c179dddb1305
kiprotichdominic <kiprotichdominic102@gmail.com>
2019-11-23 13:57:57 +0300

    import forms from forms module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,14 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { QuoteComponent } from './quote/quote.component';
-import { QuoteDetailComponent } from './quote-detail/quote-detail.component';
-import { StrikethroughDirective } from './strikethrough.directive';
-import { DateCountPipe } from './date-count.pipe';
-import { QuoteFormComponent } from './quote-form/quote-form.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { QuoteComponent } from "./quote/quote.component";
+import { QuoteDetailComponent } from "./quote-detail/quote-detail.component";
+import { StrikethroughDirective } from "./strikethrough.directive";
+import { DateCountPipe } from "./date-count.pipe";
+import { QuoteFormComponent } from "./quote-form/quote-form.component";
 
 @NgModule({
   declarations: [
@@ -18,11 +19,8 @@ import { QuoteFormComponent } from './quote-form/quote-form.component';
     DateCountPipe,
     QuoteFormComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
