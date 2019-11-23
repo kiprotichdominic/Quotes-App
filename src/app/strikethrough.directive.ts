a27e8bafc73434188c59f10df0754f1daffaceea
kiprotichdominic <kiprotichdominic102@gmail.com>
2019-11-23 11:26:10 +0300

    modify strikethough directive

--- src/app/strikethrough.directive.ts
+++ src/app/strikethrough.directive.ts
@@ -1,8 +1,19 @@
-import { Directive, ElementRef } from "@angular/core";
+import { Directive, ElementRef, HostListener } from "@angular/core";
 
 @Directive({
   selector: "[appStrikethrough]"
 })
 export class StrikethroughDirective {
   constructor(private elem: ElementRef) {}
+
+  @HostListener("click") onClicks() {
+    this.textDeco("line-through");
+  }
+  @HostListener("dblclick") onDoubleClicks() {
+    this.textDeco("none");
+  }
+
+  private textDeco(action: string) {
+    this.elem.nativeElement.style.textDecoration = action;
+  }
 }
