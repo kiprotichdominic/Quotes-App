687967107575f87f5673b8da750c89ce0b54a8fd
kiprotichdominic <kiprotichdominic102@gmail.com>
2019-11-23 13:26:41 +0300

    dd quotes

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -44,8 +44,16 @@ export class QuoteComponent implements OnInit {
     this.quotes[index].showDescription = !this.quotes[index].showDescription;
   }
 
-  completeQuote(isComplete, index) {
-    this.quotes.splice(index, 1);
+  deleteQuote(isComplete, index) {
+    if (isComplete) {
+      let toDelete = confirm(
+        `Are you sure you want to delete ${this.quotes[index].name}?`
+      );
+
+      if (toDelete) {
+        this.quotes.splice(index, 1);
+      }
+    }
   }
   constructor() {}
 
