ffd8851e7c0aae756a80ed1c807f58465769c530
kiprotichdominic <kiprotichdominic102@gmail.com>
2019-11-27 07:30:49 +0300

    add font awesome from npm

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from "@angular/core";
 import { Quote } from "../quote";
-import {trigger,  state,  style,  animate,  transition } from "@angular/animations";
 
 @Component({
   selector: "app-quote",
@@ -12,32 +11,32 @@ export class QuoteComponent implements OnInit {
   quotes: Quote[] = [
     new Quote(
       1,
-      "Watch finding Nemo",
+      "Posted by Anthony",
       "Find an online version and watch merlin find his son",
       new Date(2020, 3, 14)
     ),
     new Quote(
       2,
-      "Buy Cookies",
+      "Posted by Rose",
       "I have to buy cookies for the parrot",
       new Date(2019, 6, 9)
     ),
     new Quote(
       3,
-      "Get new Phone Case",
+      "Posted by Justus",
       "Diana has her birthday coming up soon",
       new Date(2022, 1, 12)
     ),
     new Quote(
       4,
-      "Get Dog Food",
+      "Posted By Evans",
       "Pupper likes expensive snacks",
       new Date(2019, 0, 18)
     ),
-    new Quote(5, "Solve math homework", "Damn Math", new Date(2019, 2, 14)),
+    new Quote(5, "Posted By Philiph", "Damn Math", new Date(2019, 2, 14)),
     new Quote(
       6,
-      "Plot my world domination plan",
+      "Posted by Kolum",
       "Cause I am an evil overlord",
       new Date(2030, 3, 14)
     )
